6472f4820ac7a72515608e796531ef881e69d1b2
akshit <akshitsaini97@gmail.com>
2021-05-13 03:09:34 +0530

    socket demo changes

--- src/App.js
+++ src/App.js
@@ -3,26 +3,26 @@ import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:4001";
 
 function App() {
-  const [response, setResponse] = useState("");
+  const [response, setResponse] = useState({});
 
   useEffect(() => {
     console.log("hgjg");
     const socket = socketIOClient(ENDPOINT);
     socket.emit("mnl_entry", { name: "abc", date: "98", part: "098k" });
-    socket.on("FromAPI", (data) => {
-      setResponse(data);
-    });
+    socket.on(
+      "processDone",
+      (data) => {
+        setResponse(data);
+      },
+      []
+    );
 
     // CLEAN UP THE EFFECT
     return () => socket.disconnect();
     //
   }, []);
 
-  return (
-    <p>
-      It's <time dateTime={response}>{response}</time>
-    </p>
-  );
+  return <p>{JSON.stringify(response)}</p>;
 }
 
 export default App;
